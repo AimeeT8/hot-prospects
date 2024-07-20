9e4315de1dddff575d596d629f382210f5b5d297
aimee_t <aimeetemple14@gmail.com>
2024-07-20 15:18:37 -0400

    Added initializer for filtering contacted and not contacted tabs

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -36,17 +36,37 @@ struct ProspectsView: View {
     
     var body: some View {
         NavigationStack {
-            Text("People: \(prospects.count)")
-                .navigationTitle(title)
-                .toolbar {
-                    Button("Scan", systemImage: "qrcode.viewfinder") {
-                        let prospect = Prospect(name: "Joe Smith", emailAddress: "joe@someplace.com", isContacted: false)
-                        modelContext.insert(prospect)
-                    }
+            List(prospects) { prospect in
+                VStack(alignment: .leading) {
+                    Text(prospect.name)
+                        .font(.headline)
+                    
+                    Text(prospect.emailAddress)
+                        .foregroundStyle(.secondary)
                 }
+            }
+            .navigationTitle(title)
+            .toolbar {
+                Button("Scan", systemImage: "qrcode.viewfinder") {
+                    let prospect = Prospect(name: "Joe Smith", emailAddress: "joe@someplace.com", isContacted: false)
+                    modelContext.insert(prospect)
+                }
+            }
         }
         
+        
+    }
     
+    init(filter: FilterType) {
+        self.filter = filter
+        
+        if filter != .none {
+            let showContactOnly = filter == .contacted
+            
+            _prospects = Query(filter: #Predicate {
+                $0.isContacted == showContactOnly
+            }, sort: [SortDescriptor(\Prospect.name)])
+        }
     }
 }
 
